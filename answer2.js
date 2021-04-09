ff632c58860e35be9cb4fa169e445eea13c03ecc
unknown <mlizzbert@gmail.com>
2021-04-09 16:07:37 +0100

    my first commit

---
+++ answer2.js
@@ -0,0 +1,24 @@
+//For a Consolation Winner
+
+const winnerScores = ("0-1", "3-5", "2-3", "4-1", "5-3");
+
+if (winnerScores === ("3-2", "4-4", "0-2", "4-5")) {
+    console.log("You lost the bet!");
+} else if (winnerScores === ("0-1", "3-5", "2-3", "4-1", "5-3")) {
+    console.log ("You are a Consolation Winner!!");
+} else {
+    console.log( "You are a Jackpot Winner!!!");
+}
+
+//const winnerScores = ("0-2", "4-5", "3-2", "4-4", "1-1");
+
+//For a Jackpot Winner
+//if (winnerScores === ("3-2", "4-4", "0-2", "4-5")) {
+//    console.log("You lost the bet!");
+//} else if (winnerScores === ("0-1", "3-5", "2-3", "4-1", "5-3")) {
+//    console.log ("You are a Consolation Winner!!");
+//} else {
+//    console.log( "You are a Jackpot Winner!!!");
+//}
+
+
